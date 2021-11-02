fe827825e3ef496201426d49cc4f886aad698688
Anton <bartsev.anton@gmail.com>
2021-11-02 17:32:03 +0200

    Comments and naming changes

--- controlPageScrolling.js
+++ controlPageScrolling.js
@@ -1,19 +1,18 @@
-// Number of clicks on the menu element initially set on 0
+// Number of clicks on the trigger element initially set on 0
 let numOfClicks = 0;
-// Get menu element 
-const menuTrigger = document.getElementsByClassName("MenuTriggerClassName")[0];
+// Get trigger element 
+const trigger = document.getElementsByClassName("triggerClassName")[0];
 // Get body element
 const bodyEl = document.getElementsByTagName("body")[0];
 
 
-// Set onclick function to menu element
-menuTrigger.onclick = () => {
-    // Increase number of clicks value by 1 every time the menu element is clicked
+// Set onclick function to trigger element
+trigger.onclick = () => {
     numOfClicks += 1;
     // Check if number of clicks is an even value: 
-    // odd value - menu is opened, even value - menu is closed
+    // odd value - first click, even value - second click
     const isNumOfClicksEven = numOfClicks % 2 === 0;
-    // If menu is closed - set overflow property to "auto", 
-    // If menu is opened - set overflow property to "hidden"
+    // On first click set body's overflow property to "auto", 
+    // On second click set body's overflow property to "hidden"
     isNumOfClicksEven ? bodyEl.style.overflow = "auto" : bodyEl.style.overflow = "hidden";
 };
